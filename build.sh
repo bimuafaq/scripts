160032e962508a03ea4467cb476aee37436c55f8
Bi <133272416+bimuafaq@users.noreply.github.com>
2026-07-11 07:42:36 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ sudo ln -sf "$OWN_KEYS_DIR/releasekey.x509.pem" "$OWN_KEYS_DIR/testkey.x509.pem"
 repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs --depth=1
 
 rm -rf .repo/local_manifests
-[ -d external/chromium-webview/prebuilt ] && rm -rf external/chromium-webview
 git clone -q https://github.com/bimuafaq/local_manifests .repo/local_manifests
 
+[ -d external/chromium-webview/prebuilt ] && rm -rf external/chromium-webview
 for i in 1 2; do /opt/crave/resync.sh; done
 .repo/local_manifests/patches/patches_apply.sh
 
