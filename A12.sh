bdad8a80178885ed7c7447d50b05acbe69cccf0c
Bi <133272416+bimuafaq@users.noreply.github.com>
2026-07-17 18:55:22 +0700

    Update A12.sh

--- A12.sh
+++ A12.sh
@@ -7,16 +7,17 @@ wget -q https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.
 repo init -u https://github.com/LineageOS/android.git -b lineage-19.1 --depth=1
 
 rm -rf .repo/local_manifests
+rm -rf external/chromium-webview
 git clone -q https://github.com/bimuafaq/local_manifests -b s .repo/local_manifests
 
-[ -L external/chromium-webview/Android.mk ] && rm -rf external/chromium-webview
-git -C .repo/manifests am "$(pwd)/.repo/local_manifests/patches/0001-manifest-switch-to-old-chromiun-webview.patch"
-
 for i in 1 2; do /opt/crave/resync.sh; done
+
+rm -rf external/chromium-webview
+git clone -q https://github.com/LineageOS/android_external_chromium-webview external/chromium-webview -b master --depth=1
 
 rm -rf packages/apps/FMRadio
 git clone -q https://github.com/bimuafaq/android_packages_apps_FMRadio packages/apps/FMRadio -b lineage-18.1 --depth=1
 
 source build/envsetup.sh
 lunch lineage_RMX2185-userdebug
-mka bacon
+cmka bacon
