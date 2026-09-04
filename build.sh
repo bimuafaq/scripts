d23f7aae5cf0720776bb68884702a49a94cc0dcc
Bi <133272416+bimuafaq@users.noreply.github.com>
2026-09-05 01:14:13 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,4 +6,8 @@ wget -q https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.
 
 source build/envsetup.sh
 lunch lineage_RMX2185-user
-cmka bacon
+cmka bacon && {
+    export PATH=$PWD/out/host/linux-x86/bin:$PATH
+    sign_target_files_apks -d vendor/lineage-priv/keys out/target/product/*/obj/PACKAGING/*-target_files-*.zip out/target/product/*/signed-target_files.zip
+    ota_from_target_files -k vendor/lineage-priv/keys/releasekey out/target/product/*/signed-target_files.zip out/target/product/*/signed-ota.zip
+}
