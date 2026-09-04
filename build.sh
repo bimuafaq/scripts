1a48d42204ee9626918433585862be8a6c5cdf9d
Bi <133272416+bimuafaq@users.noreply.github.com>
2026-09-05 03:08:38 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ wget -q https://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.
 source build/envsetup.sh
 lunch lineage_RMX2185-user
 cmka bacon && {
-    mka otatools
-    export PATH=$PWD/out/host/linux-x86/bin:$PATH
-    sign_target_files_apks -o -d vendor/lineage-priv/keys $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip $OUT/signed-target_files.zip
-    ota_from_target_files -k vendor/lineage-priv/keys/releasekey $OUT/signed-target_files.zip $OUT/lineage-18.1-$(date +%Y%m%d)-UNOFFICIAL-RMX2185-signed.zip
+  mka otatools
+  export PATH=$PWD/out/host/linux-x86/bin:$PATH
+  sign_target_files_apks -o -d vendor/lineage-priv/keys $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip $OUT/signed-target_files.zip
+  ota_from_target_files -k vendor/lineage-priv/keys/releasekey \
+  --block --backup=true \
+  $OUT/signed-target_files.zip \
+  $OUT/lineage-18.1-$(date +%Y%m%d)-UNOFFICIAL-RMX2185-signed.zip
 }
