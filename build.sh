03bfa622b4248807ad91e4321aee1a99a76c0d45
Bi <133272416+bimuafaq@users.noreply.github.com>
2026-09-05 03:10:11 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,7 +9,9 @@ lunch lineage_RMX2185-user
 cmka bacon && {
   mka otatools
   export PATH=$PWD/out/host/linux-x86/bin:$PATH
-  sign_target_files_apks -o -d vendor/lineage-priv/keys $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip $OUT/signed-target_files.zip
+  sign_target_files_apks -o -d vendor/lineage-priv/keys \
+    $OUT/obj/PACKAGING/target_files_intermediates/*-target_files-*.zip \
+    $OUT/signed-target_files.zip
   ota_from_target_files -k vendor/lineage-priv/keys/releasekey \
   --block --backup=true \
   $OUT/signed-target_files.zip \
